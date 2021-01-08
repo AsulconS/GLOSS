eabdf4f573eb5dfe3a7d899128585f876da87999
AsulconS <rolandobedregal321@gmail.com>
2021-01-08 12:46:49 -0500

    interrupt convention naming, syscall number now stored at r7

--- src/_swi.s
+++ src/_swi.s
@@ -7,57 +7,64 @@ button_state: .word 0x0
 _swi:
     @ Used registers:
     @ -> r0, r1
+    @ Read Only registers:
+    @ -> r7 (syscall register)
     @ Store used registers and lr onto the stack
     push {r0, r1, lr}
 
-    @ r0 will load the syscall number
+    @ r0 will load the interrupt number
     ldr r0, [lr, #-0x4]
     bic r0, r0, #0xff000000
 
-    @ Ensure valid syscall number (>=0x0 && <0x7)
-    cmp r0, #0x7
-    bge undef_syscall
+    @ Ensure valid interrupt number (>=0x0 && <0x8)
+    cmp r0, #0x8
+    bge undef_interrupt
 
-    @ Switch r0 to choose a syscall address in r1
+    @ Switch r0 to choose a interrupt address in r1
     ldr r1, jump_table_addr
     ldr r1, [r1, +r0, LSL #0x2]
 
-    @ Branch to choosen syscall
+    @ Branch to choosen interrupt
     mov pc, r1
 
-    @ 0x0 syscall
-    kill_syscall:
+    @ 0x0 interrupt
+    kill_interrupt:
         b after
 
-    @ 0x1 syscall
-    safekill_syscall:
+    @ 0x1 interrupt
+    safekill_interrupt:
         b after
 
-    @ 0x2 syscall
-    restart_syscall:
+    @ 0x2 interrupt
+    restart_interrupt:
         b after
 
-    @ 0x3 syscall
-    io_syscall:
+    @ 0x3 interrupt
+    io_interrupt:
         ldr r0, button_dma_addr
         ldr r1, button_state_addr
         ldr r0, [r0]
         str r0, [r1]
         b   after
 
-    @ 0x4 syscall
-    cse_syscall:
+    @ 0x4 interrupt
+    cse_interrupt:
         b after
 
-    @ 0x5 syscall
-    segfault_syscall:
+    @ 0x5 interrupt
+    segfault_interrupt:
         b after
 
-    @ 0x6 syscall
-    ge_syscall:
+    @ 0x6 interrupt
+    ge_interrupt:
         b after
 
-    undef_syscall:
+    @ 0x7 interrupt
+    syscall_interrupt:
+        b after
+
+    @ Default interrupt (undefined)
+    undef_interrupt:
         b after
 
 after:
@@ -65,13 +72,14 @@ after:
 
 @ Literal Pool
 jump_table:
-    .word kill_syscall
-    .word safekill_syscall
-    .word restart_syscall
-    .word io_syscall
-    .word cse_syscall
-    .word segfault_syscall
-    .word ge_syscall
+    .word kill_interrupt
+    .word safekill_interrupt
+    .word restart_interrupt
+    .word io_interrupt
+    .word cse_interrupt
+    .word segfault_interrupt
+    .word ge_interrupt
+    .word syscall_interrupt
 
 jump_table_addr:   .word jump_table
 button_dma_addr:   .word 0xff200050
